1dc8f04e014d9e5e8538d648ad5d188aad25d14a
Shawnmew <nelson.nunesnelson@hotmail.com>
2026-02-16 13:26:12 +0100

    refactor: move demo logic to controllers; add MVC-style demo controllers and fallback when DB unavailable

--- mobile/entry.js
+++
@@ -1,7 +0,0 @@
-import 'react-native-gesture-handler'
-import 'react-native-reanimated'
-import { Redirect } from 'expo-router'
-
-export default function App() {
-  return <Redirect href="/login" />
-}
